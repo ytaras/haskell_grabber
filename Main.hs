63b35968fd2aa1ed1e41306fefcfa58e53c03e76
Yura Taras <yura.taras@gmail.com>
2013-10-16 19:23:43 +0300

    Counter and cache

--- Main.hs
+++ Main.hs
@@ -6,16 +6,18 @@ import Data.ByteString.Lazy.UTF8 (toString)
 import Data.List (intercalate)
 import Text.XML.HXT.Core
 import Text.HandsomeSoup
+import Control.Monad
 
 url = "http://vpustotu.ru/moderation/"
 
 main = do
   stateRef <- atomically $ newTVar emptyState
-  q <- loadQuote
-  _ <- atomically $ addQuote conf stateRef q
+  _ <- job conf stateRef
   state <- atomically $ readTVar stateRef
-  putStrLn $ (head . elems . cache) state
-  print state
+  mapM_ putStrLn $ (elems . cache) state
+  print $ duplicates state
+  print $ counter state
+  print $ (size . cache) state
 
 
 type Quote = String
@@ -24,27 +26,36 @@ type Cache a = Set a
 data Conf = Conf { threads  :: !Int
                  , maxDupes :: !Int
                  }
-conf = Conf 1 10
+conf = Conf 1 50
 
 data State = State { cache      :: Cache Quote
                    , duplicates :: !Int
+                   , counter    :: !Int
                    } deriving Show
-emptyState = State empty 0
+emptyState = State empty 0 0
 type StateRef = TVar State
 -- TODO We're overusing state word
 data GrabberState = Continue | Quit
+
+job :: Conf -> StateRef -> IO ()
+job conf stateRef = do
+  q <- loadQuote
+  next <- atomically $ addQuote conf stateRef q
+  case next of
+    Quit     -> return ()
+    Continue -> job conf stateRef
 
 loadQuote :: IO Quote
 loadQuote = do
   bytes <- simpleHttp url
   let
     text = toString bytes
-    doc  = readString [withParseHTML yes, withWarnings no] text
+    doc  = readString [withParseHTML yes, withWarnings yes] text
   quotes <- runX $ doc >>> css "div[class='fi_text']" >>> deep getText
   return $ intercalate " " quotes
 
 nextStep :: Conf -> State -> GrabberState
-nextStep c s = if (duplicates s) >= (maxDupes c)
+nextStep c s = if (counter s) >= (maxDupes c)
                then Quit
                else Continue
 
@@ -58,8 +69,11 @@ addQuote conf sr q = do
 updateState :: Quote -> State -> State
 updateState quote state =
   if duplicate
-  then state { duplicates = (duplicates state) + 1}
-  else State { cache = (insert quote oldCache), duplicates = 0 }
+  then state { duplicates = (duplicates state) + 1,
+               counter = (counter state) + 1 }
+  else state { cache = (insert quote oldCache),
+               duplicates = 0,
+               counter = (counter state) + 1 }
   where
     duplicate = member quote oldCache
     newCache = insert quote oldCache

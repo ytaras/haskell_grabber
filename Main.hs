9d1e6dee90bd2cf0cd7d9e0425a5169efcda5240
Yura Taras <yura.taras@gmail.com>
2013-10-25 17:35:31 +0300

    Start workers fn

--- Main.hs
+++ Main.hs
@@ -1,16 +1,47 @@
 {-# LANGUAGE NoMonomorphismRestriction, DeriveDataTypeable #-}
-import System.Console.CmdArgs.Implicit
 
+import Control.Applicative
+import Control.Exception (finally)
+import Control.Concurrent
+import Control.Concurrent.STM
+import Control.Monad
+import qualified Data.Set as S
+import System.Console.CmdArgs.Implicit
 defaultUrl = "http://vpustotu.ru/moderation/"
 
-data Config = Config { url        :: String
-                     , maxThreads :: Int
-                     , dumpPeriod :: Int
+type Quote = String
+data Config = Config { url           :: String
+                     , threads       :: Int
+                     , maxDuplicates :: Int
+                     , dumpPeriod    :: Int
                      } deriving (Show, Data, Typeable)
 
+data ProgramState = Running | Stopping
+
+data Runtime = Runtime { quotes     :: TVar (S.Set Quote)
+                       , duplicates :: TVar (Int)
+                       , state      :: TVar (ProgramState)
+                       }
+
 config = Config { url = defaultUrl &= help "URL to grab from"
-                , maxThreads = 5 &= help "Max threads"
-                , dumpPeriod = 2 &= help "Show progress every N seconds"
+                , threads = 5 &= help "Max threads"
+                , maxDuplicates = 200 &= explicit &= name "duplicates" &= name "d"
+                                  &= help "Duplicates threshold to stop"
+                , dumpPeriod = 2 &= explicit &= name "showEvery" &= name "s"
+                               &= help "Show progress every N seconds"
                 } &= summary "Quotes Grabber"
 
 main = print =<< cmdArgs config
+
+prepareRuntime = Runtime <$> newTVar S.empty <*> newTVar 0 <*> newTVar Running
+
+startWorkers :: Int -> IO () -> IO ()
+startWorkers count job = do
+  counter <- atomically $ newTVar 0
+  replicateM_ count $ run counter
+  atomically $ waitForZero counter
+  where
+    run c = forkIO $ job `finally` (atomReplace c (+1))
+    atomReplace c = atomically . modifyTVar c
+    waitForZero c = readTVar c >>= \x ->
+      if x == 0 then return () else retry
